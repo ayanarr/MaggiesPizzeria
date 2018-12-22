ff71fe554ef4fe539bb4b347059264edea0f0663
ayanarr <32066709+ayanarr@users.noreply.github.com>
2018-12-21 19:05:41 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,14 @@
+$("#toggleButton1").click(function(){
+  $(".food").fadeToggle();
+});
+$("#toggleButton2").click(function(){
+  $(".drinks").fadeToggle();
+});
+
+$("#bags").click(function(){
+    $("img").attr("src","https://cdn2.bigcommerce.com/server3000/l5dryyv/products/803/images/1042/16x6x19__51051.1366675262.215.215.png?c=2");
+});
+
+$("#bags").click(function(){
+    $("#ss").text("Thank You! Come Again.");
+});
